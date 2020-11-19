648b58b03a478e56de5e3091b6fde9df75430b3d
pat-github <patcharees@hotmail.com>
2020-11-19 11:23:15 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -629,6 +629,12 @@ if(!is_null($events)){
                                             'ทำความรู้จัก ETL',
                                             'https://bcplineoa.bangchak.co.th/powerbi-club/uploadimage/c5.png',
                                             $actionBuilder5
+                                        ),
+                                        new CarouselColumnTemplateBuilder(
+                                            'EP.07',
+                                            'ทำความรู้จัก DAX',
+                                            'https://bcplineoa.bangchak.co.th/powerbi-club/uploadimage/c1.png',
+                                            $actionBuilder5
                                         ),                                         
                                     )
                                 )
